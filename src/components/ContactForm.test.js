3b91b0777293833f3730e4bb759da18e84c3e77f
monicasalas86 <monicsalas167@gmail.com>
2021-10-01 00:21:11 -0500

    all tests passing

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -144,5 +144,35 @@ test('renders all firstName, lastName and email text when submitted. Does NOT re
 });
 
 test('renders all fields text when all fields are submitted.', async () => {
-    
+    render(<ContactForm/>);
+
+    const firstNameInput = screen.getByLabelText('First Name*');
+    userEvent.type(firstNameInput, 'monica');
+
+    const lastNameInput = screen.getByLabelText('Last Name*');
+    userEvent.type(lastNameInput, 'salas');
+
+    const emailInput = screen.getByLabelText('Email*');
+    userEvent.type(emailInput, 'this@this.com');
+
+    const messageInput = screen.getByLabelText('Message');
+    userEvent.type(messageInput, 'testing the message input');
+
+    const submit = screen.getByRole('button');
+    userEvent.click(submit);
+
+    await waitFor(() => {
+        const firstName = screen.queryByTestId('firstnameDisplay')
+        expect(firstName).toBeInTheDocument();
+
+        const lastName = screen.queryByTestId('lastnameDisplay')
+        expect(lastName).toBeInTheDocument();
+
+        const email = screen.queryByTestId('emailDisplay')
+        expect(email).toBeInTheDocument();
+
+        const message = screen.queryByTestId('messageDisplay')
+        expect(message).toBeInTheDocument();
+
+    })
 });
